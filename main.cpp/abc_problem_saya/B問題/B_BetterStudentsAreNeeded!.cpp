a2c74f0651aa918e5d9acc820b80758515d907de
saya_38_t <sayabear30@gmail.com>
2025-08-29 13:47:16 +0900

    BetterStudentsAreNeeded!とけた

--- main.cpp/abc_problem_saya/B問題/B_BetterStudentsAreNeeded!.cpp
+++ main.cpp/abc_problem_saya/B問題/B_BetterStudentsAreNeeded!.cpp
@@ -3,26 +3,24 @@ using namespace std;
 #define rep(i,n) for(int i=0; i<(n);++i)
 using ll = long long;
 
-vector<pair<int,int>> num(vector<pair<int,int>>A){
+void num(vector<pair<int,int>>& A){
     int j=1;
     rep(i,A.size()){
-        A[i].second = j;
+        A[i].second = -j;
         j++;
-    }
-    return A;
+    };
 }
 
-vector<bool> solve(vector<pair<int,int>>A, vector<bool>ans, int x){
+void solve(vector<pair<int,int>>& A, vector<bool>& ans, int x){
     sort(A.rbegin(), A.rend());
     int i=0;
-    while(x>-1){
-        if(ans[A[i].second]==false){
-            ans[A[i].second]=true;
+    while(x>0){
+        if(ans[-A[i].second]==false){
+            ans[-A[i].second]=true;
             x--;
         }
         i++;
     }
-    return ans;
 }
 
 int main(){
@@ -32,18 +30,21 @@ int main(){
     vector<pair<int,int>>B(n);
     vector<pair<int,int>>AB(n);
 
-    A=num(A); B=num(B); AB=num(AB);
+    num(A); num(B); num(AB);
 
     rep(i,n) cin >> A[i].first;
     rep(i,n) cin >> B[i].first;
     rep(i,n) AB[i].first = A[i].first + B[i].first;
 
-    vector<bool>ans(n+1,false);
+    vector<bool>ans(n+1);
+    rep(i,n+1) ans[i] = false;
 
-    ans=solve(A,ans,x); ans=solve(B,ans,y); ans=solve(AB,ans,z);
+    solve(A,ans,x);
+    solve(B,ans,y);
+    solve(AB,ans,z);
 
     rep(i,n+1){
-        if(ans[i+1]) cout << i+1 << '\n';
-    }
+        if(ans[i]) cout << i << '\n';
+    };
     return 0;
 }
