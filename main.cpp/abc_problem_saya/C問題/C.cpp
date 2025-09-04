e11257e96a3d630e362d87f1ab4263f350727778
saya_38_t <sayabear30@gmail.com>
2025-09-04 19:41:11 +0900

    ADT20250904vol2

--- main.cpp/abc_problem_saya/C問題/C.cpp
+++ main.cpp/abc_problem_saya/C問題/C.cpp
@@ -6,6 +6,31 @@ using P = pair<int,int>;
 const double PI = acos(-1);
 
 int main(){
-    
+    int n,k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vector<bool>teeth(n);
+    rep(i,n) {
+        if(s[i]=='O') teeth[i]=true;
+    }
+    int cnt = 0;
+    int ck = k;
+    rep(i,n){
+        if(ck>0 && teeth[i]) ck--;
+        else if(ck>0 && !teeth[i]) {
+            ck = k;
+            continue;
+        }
+        if(ck==0){
+            cnt++;
+            for(int j=i;j>i-k+1;j--){
+                teeth[i] = false;
+            }
+            ck = k;
+        }
+    }
+    cout << cnt << endl;
+    return 0;
 }
 /**/
--- main.cpp/abc_problem_saya/D問題/D.cpp
+++ main.cpp/abc_problem_saya/D問題/D.cpp
@@ -6,5 +6,29 @@ using P = pair<int,int>;
 const double PI = acos(-1);
 
 int main(){
+    int n,q;
+    cin >> n >> q;
+    vector<int>x(q);
+    rep(i,q) cin >> x[i];//ボール
+    vector<P>box(n);//(数、箱の番号ー１)
+    rep(i,n) box[i].second = i;//あとで1タス
+    vector<int>ball(q);//ボール配列
+    rep(i,q){
+        if(x[i]!=0){
+            rep(j,n){
+                if(box[j].second == (x[i]-1)) box[j].first++;
+                else continue;
+            }
+            ball[i] = x[i];
+        }
+        else{
+            sort(box.begin(),box.end());
+            box[0].first++;
+            ball[i]=box[0].second+1;
+        }
+    }
+    rep(i,q) cout << ball[i] << ' ';
+    cout << endl;
+    return 0;
 }
 /**/
