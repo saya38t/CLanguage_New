afeb1cb801ab2ab61a73bffdcde03d04f2974a94
saya_38_t <sayabear30@gmail.com>
2025-09-12 00:34:01 +0900

    ADT20250911AC

--- main.cpp/abc_problem_saya/C2.cpp
+++ main.cpp/abc_problem_saya/C2.cpp
@@ -7,21 +7,20 @@ const double PI = acos(-1);
 using db = double;
 
 int main(){
-    double ans = 0;
     int n;
     cin >> n;
     vector<double>x(n+1);
     vector<double>y(n+1);//
     rep(i,n) cin >> x[i+1] >> y[i+1];
+    double ans = 0;
     for(int i=0; i<n+1;i++){
-        double X=(double)x[i]-x[i+1];
-        double Y=(double)y[i]-y[i+1];
-        double a =X*X;
+        double X=x[i+1]-x[i];
+        double Y=y[i+1]-y[i];
+        double a=X*X;
         double b=Y*Y;
         ans += sqrt(a+b);
-        cout << ans<< endl;
     }
-    printf("%18.18g\n",ans);//小数点以下16桁で出力(\nで改行)
+    printf("%.10f\n",ans);
     return 0;
 }
 /**/
