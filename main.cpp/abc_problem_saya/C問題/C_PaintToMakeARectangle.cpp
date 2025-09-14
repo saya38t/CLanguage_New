81d9775baa79c8eaf5f8b6200397083b8460005a
saya_38_t <sayabear30@gmail.com>
2025-09-14 19:03:55 +0900

    painttomakearectangle

--- main.cpp/abc_problem_saya/C問題/C_PaintToMakeARectangle.cpp
+++ main.cpp/abc_problem_saya/C問題/C_PaintToMakeARectangle.cpp
@@ -8,5 +8,18 @@ const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
+    int n;
+    ll L,W;
+    cin >> n >> L >> W;
+    vector<ll>a(n+1,L);
+    rep(i,n) cin >> a[i];
+    ll r=0LL;//longlongを明確に固定
+    ll ans=0LL;
+    rep(i,n+1){
+        if(r<a[i]) ans+=(a[i]-r+W-1)/W;//%=0はwを足したくないので-1
+        r=a[i]+W;
+    }
+    cout << ans << endl;
+    return 0;
 }
 /*解く！！*/
