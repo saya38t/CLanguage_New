76e21a8d5118c575e3dcf666dfc9344928ec4e57
saya_38_t <sayabear30@gmail.com>
2025-09-14 20:37:46 +0900

    prime

--- main.cpp/abc_problem_saya/D問題/D_PrimeSumGame.cpp
+++ main.cpp/abc_problem_saya/D問題/D_PrimeSumGame.cpp
@@ -8,6 +8,25 @@ const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
-    
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    for(int i=a; i<b+1; i++){
+        int t = 0;
+        for(int j=c;j<d+1; j++){
+            int sum=i+j;
+            for(int k=2; k<sum; k++){
+                if(sum%k==0){
+                    t++;
+                    break;
+                }
+            }
+        }
+        if(t==d-c+1) {
+            cout << "Takahashi" << endl;
+            return 0;
+        }
+    }
+    cout << "Aoki" << endl;
+    return 0;
 }
 /*解く！！*/
