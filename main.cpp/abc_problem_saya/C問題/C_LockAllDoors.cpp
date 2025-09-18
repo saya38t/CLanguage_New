3a9282699c4a189739faa33d1b507082b0a56595
saya_38_t <sayabear30@gmail.com>
2025-09-18 22:49:11 +0900

    LockAllDoors

--- main.cpp/abc_problem_saya/C問題/C_LockAllDoors.cpp
+++ main.cpp/abc_problem_saya/C問題/C_LockAllDoors.cpp
@@ -1,7 +1,7 @@
+#define _GLIBCXX_DEBUG//配列外参照防止
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for(int i=0; i<(n); ++i)
-#define _GLIBCXX_DEBUG//配列外参照防止
 using ll = long long;
 using P = pair<int,int>;
 const double PI = acos(-1); //π
@@ -13,31 +13,18 @@ int main(){
     cin >> n >> r;
     vector<int>l(n);
     rep(i,n) cin >> l[i];
-    ll ans = 0;
+    r--;
     int R=r,L=r;
-    if(r>n-1) {
-        R=n-1; L=n-1;
+    rep(i,n){
+      if(l[i]==1) continue;
+      L=min(L,i-1);//左
+      R=max(R,i);//右
     }
-    rep(i,r+1) {//左～R
-        if(l[i]==0){
-            L=i;
-            break;
-        }
-    }
-    for(int i=n-1; i>r-1;i--){//右～R
-        if(l[i]==0){
-            R=i;
-            break;
-        }
-    }
-    if(L==R && l[L]==1){
-        cout << 0 << endl;
-        return 0;
-    }
-    for(int i=L; i<R+1; i++){
+    ll ans = 0;
+    for(int i=L+1; i<R+1; i++){
+      //開始値と終了値がズレるので全部1とかl[r]==0を気にしなくていい
         ans+=(l.at(i)+1);
     }
     cout << ans << endl;
     return 0;
 }
-/*言い換えてシンプルに解く*/
