d60f89f294d1076ad23d50e5e9ac73e130cd12db
saya_38_t <sayabear30@gmail.com>
2025-09-19 21:42:34 +0900

    iroiro

--- main.cpp/abc_problem_saya/C問題/C_RepunitTrio.cpp
+++ main.cpp/abc_problem_saya/C問題/C_RepunitTrio.cpp
@@ -9,5 +9,23 @@ const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
+    int n;
+    cin >> n;
+    vector<ll>runt;
+    for(ll i=1; i<1e12; i=10*i+1){
+        runt.emplace_back(i);
+    }
+    set<ll>st;
+    for(ll a:runt){
+        for(ll b:runt){
+            for(ll c:runt){
+                st.emplace(a+b+c);
+            }
+        }
+    }
+    vector<ll>all;
+    for(ll d:st) all.emplace_back(d);
+    cout << all.at(n-1) << endl;
+    return 0;
 }
 /*言い換えてシンプルに解く*/
