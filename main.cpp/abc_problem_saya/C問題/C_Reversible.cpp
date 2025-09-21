cf4ccbbfd0055f7d6a77ea25295f93da3fcc9058
saya_38_t <sayabear30@gmail.com>
2025-09-21 23:02:34 +0900

    reversible

--- main.cpp/abc_problem_saya/C問題/C_Reversible.cpp
+++ main.cpp/abc_problem_saya/C問題/C_Reversible.cpp
@@ -9,5 +9,29 @@ const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
+    int n;
+    cin >> n;
+    set<string>st;
+    rep(i,n){
+        string s;
+        cin >> s;
+        st.emplace(s);
+    }
+    int l = st.size();
+    vector<string>S;
+    for(string a:st) S.emplace_back(a);
+    int cnt= n-l;//
+    int p = 0;
+    rep(i,l){
+        string s=S[i];
+        string rs=S[i];
+        reverse(rs.begin(),rs.end());
+        if(rs==s) p++;
+        st.emplace(rs);
+    }
+    cnt += (2*l-st.size()-p)/2;
+    int ans = n-cnt;
+    cout << ans << endl;
+    return 0;
 }
 /*言い換えてシンプルに解く*/
