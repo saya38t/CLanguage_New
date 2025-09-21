89c3483d4293fe90a9daa67a09b2133c49d93559
saya_38_t <sayabear30@gmail.com>
2025-09-22 01:37:05 +0900

    RANDOM

--- main.cpp/abc_problem_saya/C問題/C_RANDOM.cpp
+++ main.cpp/abc_problem_saya/C問題/C_RANDOM.cpp
@@ -9,5 +9,27 @@ const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
+    int h,w;
+    cin >> h >> w;
+    vector<string>S(h);
+    vector<string>T(h);
+    rep(i,h) cin >> S[i];
+    rep(i,h) cin >> T[i];
+    vector<string>rs(w);
+    vector<string>rt(w);
+    rep(j,w)rep(i,h){
+        rs[j]+=S[i][j];
+        rt[j]+=T[i][j];
+    }
+    sort(rs.begin(),rs.end());
+    sort(rt.begin(),rt.end());
+    rep(i,w){
+        if(rs[i]!=rt[i]){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
 }
 /*言い換えてシンプルに解く*/
