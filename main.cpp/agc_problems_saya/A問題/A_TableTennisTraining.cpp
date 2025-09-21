13e2c04e7e9b3828f77b47a8f0d348ecbb12b2c3
saya_38_t <sayabear30@gmail.com>
2025-09-22 01:07:55 +0900

    TTT

--- main.cpp/agc_problems_saya/A問題/A_TableTennisTraining.cpp
+++ main.cpp/agc_problems_saya/A問題/A_TableTennisTraining.cpp
@@ -9,6 +9,13 @@ const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
-    
+    ll n,a,b;
+    cin >> n >> a >> b;
+    ll l=abs(b-a);
+    ll ans;
+    if(l%2==0) ans = l/2;
+    else ans=min(a+(b-a-1)/2,n-b+1+(-a+b-1)/2);
+    cout << ans << endl;
+    return 0;
 }
 /*言い換えてシンプルに解く*/
