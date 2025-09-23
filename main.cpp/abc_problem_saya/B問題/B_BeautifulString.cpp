8e76014f1be6f5ea11ff7c3ae714c63f3863dfd8
saya_38_t <sayabear30@gmail.com>
2025-09-23 10:58:51 +0900

    BS

--- main.cpp/abc_problem_saya/B問題/B_BeautifulString.cpp
+++ main.cpp/abc_problem_saya/B問題/B_BeautifulString.cpp
@@ -9,5 +9,26 @@ const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
+    string w;
+    cin >> w;
+    int n=w.size();
+    vector<char>vec(n);
+    rep(i,n) vec[i]=w[i];
+    sort(vec.begin(),vec.end());
+    char sm=vec[0];
+    int cnt = 0;
+    rep(i,n){
+        if(vec[i]==sm){
+            cnt++;
+        }
+        else{
+            if(cnt%2!=0) break;
+            sm=vec[i];
+            cnt=1;
+        }
+    }
+    if(cnt%2==0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
 }
 /*言い換えてシンプルに解く*/
