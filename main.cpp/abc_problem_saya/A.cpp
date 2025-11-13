e54d75a4d6c09d1f5d91d1497a299a7ce7a3af2c
saya_38_t <sayabear30@gmail.com>
2025-11-13 22:51:30 +0900

    ABreset

--- main.cpp/abc_problem_saya/A.cpp
+++ main.cpp/abc_problem_saya/A.cpp
@@ -1,47 +1,17 @@
-#define _GLIBCXX_DEBUG//配列外参照防止
+#ifndef ONLINE_JUDGE
+  #define _GLIBCXX_DEBUG//配列外参照防止
+#endif
 #include <bits/stdc++.h>
+#include <unordered_set>
 using namespace std;
-#define rep(i,n) for(int i=0; i<(n); ++i)
 using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
 using P = pair<int,int>;
 const double PI = acos(-1); //π
 const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
-    int n,q;
-    cin >> n >> q;
-    vector<int>A(n);
-    rep(i,n) cin >> A[i];
-    //累積w
-    vector<int>ims(n+1);
-    rep(i,n) ims[i+1]=A[i];
-    rep(i,n) ims[i+1]+=ims[i];
-    //
-    ims.emplace_back(0);
-    int m=0;
-    const int mod=n+1;
-    rep(i,q){
-        int num; cin >> num;
-        if(num==1){
-            int c;cin >> c;
-            m+=c%mod;
-            m=(m+mod)%mod;//
-        }
-        else{
-            int l,r;
-            cin >> l >> r;
-            int L,R;
-            L=(l+m+mod)%mod;
-            R=(r+m+mod)%mod;
-            //
-            if(ims[R]-ims[L-1]>0) cout << ims[R]-ims[L-1] << endl;
-            else {
-                cout << -(ims[R]-ims[L+1])+ims[R] << endl;
-                //cout << L << ',' << R<< endl;
-            }
-        }
-    }
-    return 0;
+
+  return 0;
 }
-/*言い換えてシンプルに解く*/
--- main.cpp/abc_problem_saya/B.cpp
+++ main.cpp/abc_problem_saya/B.cpp
@@ -2,29 +2,16 @@
   #define _GLIBCXX_DEBUG//配列外参照防止
 #endif
 #include <bits/stdc++.h>
+#include <unordered_set>
 using namespace std;
-#define rep(i,n) for(int i=0; i<(n); ++i)
 using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
 using P = pair<int,int>;
 const double PI = acos(-1); //π
 const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<int>dj={1,-1,0,1,-1,-1,1,0};
 
 int main(){
-  vector<ll>cinq(36),trois(40);
-  cinq[0]=1; trois[0]=1;
-  rep(i,35) cinq[i+1]=cinq[i]*5;
-  rep(i,39) trois[i+1]=trois[i]*3;
-  ll n; cin >> n;
-  rep(i,35){
-    rep(j,39){
-      if(cinq[i+1]+trois[j+1]==n){
-        cout << j+1 << ' ' << i+1 << endl;
-        return 0;
-      }
-    }
-  }
-  cout << -1 << endl;
+
   return 0;
 }
-/*beetkiller...なんでえ*/
