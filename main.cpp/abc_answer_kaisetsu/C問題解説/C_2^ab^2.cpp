48d0898196af7e3fdb6598688b9868c5b3b20f21
saya_38_t <sayabear30@gmail.com>
2025-12-11 23:09:14 +0900

    ast

---
+++ main.cpp/abc_answer_kaisetsu/C問題解説/C_2^ab^2.cpp
@@ -0,0 +1,27 @@
+#ifndef ONLINE_JUDGE
+  #define _GLIBCXX_DEBUG//配列外参照防止
+#endif
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+using P = pair<ll,ll>;
+//a=min(a,b)、a=max(a,b)
+template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
+template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+const double PI = acos(-1); //π
+const vector<int>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
+const vector<int>dj={1,-1,0,1,-1,-1,1,0};
+
+int main(){
+  ll n; cin >> n;
+  ll ans=0;
+  while(n>0){
+    n/=2;
+    ll r=sqrtl(n);
+    ans+=(r+1)/2;
+  }
+  cout << ans << endl;
+  return 0;
+}
