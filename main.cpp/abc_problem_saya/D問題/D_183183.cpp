281a77918aac63f3c796068d19fc4cffd34ba4d9
saya38t <sayabeay30@gmail.com>
2026-01-03 18:06:18 +0900

    ast

--- main.cpp/abc_problem_saya/D問題/D_183183.cpp
+++ main.cpp/abc_problem_saya/D問題/D_183183.cpp
@@ -20,6 +20,28 @@ const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
 
 //longlong仕様
 int main(){
+  ll n,m ; cin >> n >> m;
+  vector<string>A(n);
+  rep(i,n) cin >> A[i];
+  vector<map<ll,vector<ll>>>mp(11);
+  vector<ll>ten(11);
+  ten[0]=1;
+  rep(i,10) ten[i+1]=ten[i]*10;
+  rep(i,n){//mp[桁数][mod]=vec(i)
+    ll x=A[i].size();
+    mp[x][stoll(A[i])%m].emplace_back(i);
+  }
+  ll cnt =0;
+  rep(i,n){
+    for(ll x=1; x<=10; x++){
+      //cout << mp[x][0].size() << endl;
+      if(mp[x].size()==0) continue;
+      ll M=m-((ten[x]%m)*(stoll(A[i])%m)+m)%m;
+      if(M==m) M=0;
+      if(mp[x].count(M)) cnt+=mp[x][M].size();
+    }
+  }
+  cout << cnt << endl;
   return 0;
 }
 /*
--- main.cpp/abc_problem_saya/D問題/D_TailOfSnake.cpp
+++ main.cpp/abc_problem_saya/D問題/D_TailOfSnake.cpp
@@ -20,8 +20,26 @@ const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
 
 //longlong仕様
 int main(){
+  ll n; cin >> n;
+  vector<ll>A(n),B(n),C(n);
+  rep(i,n) cin >> A[i];
+  rep(i,n) cin >> B[i];
+  rep(i,n) cin >> C[i];
+  vector<vector<ll>>dp(3,vector<ll>(n,-1));
+  dp[0][0]=A[0];
+  rep(i,3)rep(j,n-1){
+    if(dp[i][j]==-1) continue;
+    //A
+    if(i==0 && j<n-2) chmax(dp[i][j+1],dp[i][j]+A[j+1]);//そのまま
+    if(i==0) chmax(dp[i+1][j+1],dp[i][j]+B[j+1]);
+    //B
+    if(i==1) {
+      chmax(dp[i][j+1],dp[i][j]+B[j+1]);//そのまま
+      chmax(dp[i+1][j+1],dp[i][j]+C[j+1]);
+    }
+    //c
+    if(i==2) chmax(dp[i][j+1],dp[i][j]+C[j+1]);//そのまま
+  }
+  cout << dp[2][n-1] << endl;
   return 0;
 }
-/*
-auto f=[&](vector<int>A, int x){//ラムダ式}
-*/
