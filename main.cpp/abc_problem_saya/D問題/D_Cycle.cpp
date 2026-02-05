b76fd86d9504dc2fc0881361bab97bfb28a8ce8b
saya38t <sayabeay30@gmail.com>
2026-02-05 10:07:16 +0900

    D_Cycle

--- main.cpp/abc_problem_saya/D問題/D_Cycle.cpp
+++ main.cpp/abc_problem_saya/D問題/D_Cycle.cpp
@@ -20,8 +20,28 @@ const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
 
 //longlong仕様
 int main(){
+  ll n,m; cin >> n >> m ;
+  vector<vector<ll>>gra(n+1);
+  rep(i,m){
+    ll a,b; cin >> a >> b;
+    a--; b--;
+    if(b==0) gra[a].emplace_back(n);
+    else gra[a].emplace_back(b);
+  }
+  //bfs
+  vector<ll>dist(n+1,-1);
+  dist[0]=0;
+  queue<ll>Q;
+  Q.push(0);
+  while(!Q.empty()){
+    ll t=Q.front(); Q.pop();
+    for(ll p: gra[t]){
+      if(dist[p]!=-1) continue;
+      if(p==n) chmin(dist[n],dist[t]+1);
+      dist[p]=dist[t]+1;
+      Q.push(p);
+    }
+  }
+  cout << dist[n] << endl;
   return 0;
 }
-/*
-auto f=[&](vector<int>A, int x){//ラムダ式}
-*/
