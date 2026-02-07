4995c781f73cf6e103640811f2770f8e53a403d9
saya38t <sayabeay30@gmail.com>
2026-02-07 23:11:07 +0900

    B_Digit Sum

---
+++ main.cpp/abc_problem_saya/B問題/B_Digit Sum.cpp
@@ -0,0 +1,39 @@
+#ifndef ONLINE_JUDGE
+  #define _GLIBCXX_DEBUG//配列外参照防止
+#endif
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+/*
+#include <atcoder/all>
+using namespace atcoder;//必要なときだけ
+*/
+using ll = long long;
+#define rep(i,n) for(ll i=0; i<(n); ++i)
+using P = pair<ll,ll>;
+//a=min(a,b)、a=max(a,b)
+template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
+template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+const double PI = acos(-1); //π
+const vector<ll>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
+const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
+
+//longlong仕様
+int main(){
+  ll n,k; cin >> n >> k;
+  ll cnt=0;
+  for(ll i=1; i<=n; i++){
+    ll N=i;
+    ll sum=0;
+    while(N>0){
+      sum+=N%10;
+      N/=10;
+    }
+    if(sum==k) cnt++;
+  }
+  cout << cnt << endl;
+  return 0;
+}
+/*
+auto f=[&](vector<int>A, int x){//ラムダ式}
+*/
