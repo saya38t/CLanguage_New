7e5ec910958719ae044216ba6f58573e15b48207
saya38t <sayabeay30@gmail.com>
2026-02-09 22:34:48 +0900

    awsD

--- main.cpp/abc_problem_saya/D問題/D_街道の承認.cpp
+++ main.cpp/abc_problem_saya/D問題/D_街道の承認.cpp
@@ -20,7 +20,26 @@ const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
 
 //longlong仕様
 int main(){
-  
+  ll n,m,k; cin >> n >> m >> k;
+  vector<ll>A(n),B(n);
+  rep(i,n) cin >> A[i] >> B[i];
+  ll ans=0;
+  //dp
+  vector<vector<ll>>dp(n,vector<ll>(m+1,-1));//[番号][滞在費]=利益
+  rep(i,n) {
+    if(B[i]>m) continue;
+    dp[i][B[i]]=A[i];
+  }
+  rep(i,n)rep(j,m+1){
+    if(dp[i][j]==-1) continue;
+      for(ll I=i+1; I<min(i+k+1,n); I++){
+        ll J=j+B[I];
+        if(J>m) continue;
+        chmax(dp[I][J],dp[i][j]+A[I]);
+      }
+  }
+  rep(i,n)rep(j,m+1) chmax(ans,dp[i][j]);
+  cout << ans << endl;
   return 0;
 }
 /*
