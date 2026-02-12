34a361ed30540d7483514014f72b4cb7436433bc
saya38t <sayabear30@gmail.com>
2026-02-12 21:36:36 +0900

    Sum0, dp

--- main.cpp/abc_answer_kaisetsu/C問題解説/C_Sum=0.cpp
+++ main.cpp/abc_answer_kaisetsu/C問題解説/C_Sum=0.cpp
@@ -20,6 +20,27 @@ const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
 
 //longlong仕様
 int main(){
+  ll n; cin >> n;
+  vector<ll>L(n),R(n);
+  ll sumL=0,sumR=0;
+  rep(i,n){
+    cin >> L[i] >>R[i];
+    sumL+=L[i]; sumR+=R[i];
+  }
+  if(sumR<0 || sumL>0){
+    cout << "No" << endl;
+    return 0;
+  }
+  vector<ll>imos(n);
+  rep(i,n) imos[i]=R[i]-L[i];
+  rep(i,n-1) imos[i+1]+=imos[i];
+  ll x=lower_bound(imos.begin(),imos.end(),0-sumL)-imos.begin();//
+  if(x>0) sumL+=imos[x-1];
+  cout << "Yes" << endl;
+  rep(i,x) cout << R[i] << ' ';
+  cout << L[x]-sumL << ' ';
+  for(ll i=x+1; i<n; i++) cout << L[i] << ' ';
+  cout << endl;
   return 0;
 }
 /*
--- main.cpp/abc_problem_saya/D問題/D_BonusEXP.cpp
+++ main.cpp/abc_problem_saya/D問題/D_BonusEXP.cpp
@@ -20,6 +20,20 @@ const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
 
 //longlong仕様
 int main(){
+  ll n;cin >> n;
+  vector<ll>A(n);
+  rep(i,n) cin >> A[i];
+  vector<vector<ll>>dp(n+1,vector<ll>(4,0));
+  dp[0][2]=A[0];
+  rep(i,n-1){
+    dp[i+1][0]=max(dp[i][1]+2*A[i+1],dp[i][2]+2*A[i+1]);
+    dp[i+1][1]=max(dp[i][1],dp[i][2]);
+    dp[i+1][2]=max(dp[i][0]+A[i+1],dp[i][3]+A[i+1]);
+    dp[i+1][3]=max(dp[i][0],dp[i][3]);
+  }
+  ll ans=-1;
+  rep(i,4) chmax(ans,dp[n-1][i]);
+  cout << ans << endl;
   return 0;
 }
 /*
