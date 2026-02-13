53f5ca69395a8646224b65026f76ba701d9ab49f
saya38t <sayabear30@gmail.com>
2026-02-13 13:21:46 +0900

    Divide and Divide

--- main.cpp/abc_answer_kaisetsu/C問題解説/C_Divide and Divide.cpp
+++ main.cpp/abc_answer_kaisetsu/C問題解説/C_Divide and Divide.cpp
@@ -19,7 +19,17 @@ const vector<ll>di={1,1,1,0,0,-1,-1,-1};//表移動(8)
 const vector<ll>dj={1,-1,0,1,-1,-1,1,0};
 
 //longlong仕様
+//メモ化再帰
 int main(){
+  ll n; cin >> n;
+  map<ll,ll>memo;
+  auto f=[&](auto f,ll x)->ll{
+    if(memo[x]!=0) return memo[x];
+    if(x==1) return 0;
+    ll res=f(f,x/2)+f(f,(x+1)/2)+x;
+    return memo[x]=res;//こうすればメモとreturn同時にできる!
+  };
+  cout << f(f,n) << endl;
   return 0;
 }
 /*
